c2705a240145de8f2111e75f370cd666f49deab5
codergalib <88668866+codergalib@users.noreply.github.com>
2021-08-14 23:22:49 +0600

    Add files via upload

---
+++ navbar section/index.js
@@ -0,0 +1,9 @@
+            function menuBtn() {
+                var selectMenu = document.getElementById('navMenu')
+                if (selectMenu.style.display == 'block'){
+                    selectMenu.style.display = 'none'
+                } else {
+                    selectMenu.style.display = 'block'
+                }
+
+             }
